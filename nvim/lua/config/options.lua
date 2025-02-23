0932a56c34ca5a222e349c27706f8534f5e5567a
mr687 <26916086+mr687@users.noreply.github.com>
2025-02-23 16:37:24 +0700

    feat(config): enhance performance and animation
    Enable animation for a more dynamic experience. Disable unused
    providers to streamline performance and reduce overhead. Embrace
    efficiency and elegance in our Neovim setup.

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -52,3 +52,9 @@ vim.cmd([[let &t_Ce = "\e[4:0m"]])
 opt.formatoptions:append({ "r" })
 
 vim.g.lazyvim_prettier_needs_config = false
+
+vim.g.snacks_animate = true
+
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
